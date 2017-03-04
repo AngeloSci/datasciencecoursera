c8b3b814170aac96f26714177896687222fee63c
AngeloSci <angelo.scicolone2@gmail.com>
2017-03-04 10:36:02 +0000

    testing dputting and dumping

--- Course2Week1.R
+++ Course2Week1.R
@@ -1,4 +1,6 @@
 
+setwd("D:/GithubLocal/datasciencecoursera/")
+
 x <- vector()
 
 x <- 1:20
@@ -17,3 +19,26 @@ p <- z*2
 
 cbind(z,p)
 rbind(z,p)
+
+
+fc <- factor(c("male","female","female","male","female"))
+table(fc)
+unclass(fc)
+
+fl <- factor(c("high","very high","low","high"), levels = c("very high","high","low"))
+unclass(fl)
+
+nrow() 
+ncol()  #for data frames
+
+dimnames()  #list of names for rows and cols of a MATRIX
+
+sapply() #apply function over list or vector
+
+
+dput(x, file = "puttingTest.R")
+dget("puttingTest.R")
+
+dump(c("x","y"), file = "dumpingTest.R")
+rm(x,y)
+source("dumpingTest.R")
