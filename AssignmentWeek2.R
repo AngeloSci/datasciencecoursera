0e4adba7160a55e52a24a578e7fd2b2e7f61ae9a
AngeloSci <angelo.scicolone2@gmail.com>
2017-03-08 20:55:34 +0000

    Started part 1 of the assignment

--- AssignmentWeek2.R
+++ AssignmentWeek2.R
@@ -1,3 +1,33 @@
 rm(list = ls())
 
 setwd("D:/GithubLocal/datasciencecoursera/")
+
+
+
+# PART 1
+
+pollutantmean <- function(directory = "specdata", pollutant, id = 1:332){
+    
+    pollutant <- "nitrate"
+    directory = "specdata"
+    id = 175
+    lnid <- length(id)
+    
+    for(ii in 1:lnid){
+    
+        if(id[ii] < 10){
+            strid <- paste("00",as.character(id[ii]), sep="")
+        }else if (id[ii]<100){
+            strid <- paste("0",as.character(id[ii]), sep="")
+        }else{
+            strid <- as.character(id[ii])
+        }    
+        
+       data<-  read.csv(paste(directory,"/",strid,".csv",sep=""))
+    }
+    
+    
+    
+    
+    
+}
