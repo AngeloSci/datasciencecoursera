6b487f0f961fb3c89d0d2579a5b9234f2a64aa2f
AngeloSci <angelo.scicolone2@gmail.com>
2017-03-08 20:50:55 +0000

    Week 2 quiz done

--- Course2Week2.R
+++ Course2Week2.R
@@ -4,7 +4,7 @@ setwd("D:/GithubLocal/datasciencecoursera/")
 above <- function (x, n=10){
     use <- x > n
     x[use]
-  
+    
 }
 
 
@@ -12,3 +12,42 @@ x = 1:50
 
 above(x)
 above(x,25)
+
+
+#QUIZ
+cube <- function(x, n) {
+    x^3
+}
+
+cube(3)
+
+
+x <- 1:2
+if(x > 5) {
+    x <- 0
+}
+
+
+rm(x)
+
+
+f <- function(x) {
+    g <- function(y) {
+        y + z
+    }
+    z <- 4
+    x + g(x)
+}
+
+z <- 10
+f(3)
+
+
+rm(x,y)
+
+x <- 5
+y <- if(x < 3) {
+    NA
+} else {
+    10
+}
