947b7f5f8f450e49ef17b4a1e4b3424949432005
AngeloSci <angelo.scicolone2@gmail.com>
2017-03-18 11:48:30 +0000

    going fwd with week 3

--- Course2Week3.R
+++ Course2Week3.R
@@ -19,3 +19,28 @@ lapply(x,function(asd) asd[,2])
 x<- 1:5
 lapply(x,mean)
 sapply(x,mean)
+
+list(rep(1,4),rep(2,3),rep(3,2),rep(4,1))
+
+mapply(rep,1:4,4:1)
+
+mapply(rnorm, 1:5,1:5,2)
+
+x<- c(rnorm(10), runif(10),rnorm(10,1))
+f<- gl(3,10)
+tapply(x,f,mean)
+
+split(x,f)
+
+lapply(split(x,f),mean)
+
+
+
+x<- rnorm(10)
+f1<-gl(2,5)
+f2<-gl(5,2)
+interaction(f1,f2)
+
+str(split(x,list(f1,f2)))
+
+str(split(x,list(f1,f2),drop = T))
