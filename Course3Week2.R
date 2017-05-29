3c7418e466c8c166d5c1440d83acbf20fc810be5
AngeloSci <angelo.scicolone2@gmail.com>
2017-05-29 13:22:47 +0100

    working course 3 week 2

--- Course3Week2.R
+++ Course3Week2.R
@@ -20,3 +20,14 @@ allTables[1:5]
 dbListFields(hg19, "affyU133Plus2")
 
 dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+
+affyData <- dbReadTable(hg19, "affyU133Plus2") 
+head(affyData)
+
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 3")
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+
+affyMisSmall <- fetch(query, n=10); dbClearResult(query);
+dim(affyMisSmall)
+
+dbDisconnect(hg19)
