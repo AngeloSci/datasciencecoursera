dd0d60445fee7da1b1db9ad045cad1ff207f4df1
AngeloSci <angelo.scicolone2@gmail.com>
2017-05-29 17:54:25 +0100

    working course 3 week 2

--- Course3Week2.R
+++ Course3Week2.R
@@ -31,3 +31,67 @@ affyMisSmall <- fetch(query, n=10); dbClearResult(query);
 dim(affyMisSmall)
 
 dbDisconnect(hg19)
+
+
+#HDF5
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("rhdf5")
+
+library(rhdf5)
+
+created = h5createFile("example.h5")
+created
+created = h5createGroup("example.h5","foo")
+created = h5createGroup("example.h5","baa")
+created = h5createGroup("example.h5","foo/foobaa")
+
+h5ls("example.h5")
+
+A = matrix(1:10, nr=5,nc=2)
+h5write(A, "example.h5","foo/A")
+B = array(seq(0.1,2,by=0.1), dim=c(5,2,2))
+attr(B, "scale") <- "liter"
+h5write(B, "example.h5","foo/foobaa/B")
+h5ls("example.h5")
+
+df = data.frame(1L:5L,seq(0.1,1,length.out = 5),c("ab","cde","fghi","a","s"), stringsAsFactors = F)
+h5write(df, "example.h5","df")
+h5ls("example.h5")
+
+readdf = h5read("example.h5","df")
+readdf
+
+# write 12,13,14 in the first three rows and first column of dataset foo/A
+h5write(c(12,13,14), "example.h5","foo/A",index=list(1:3,1))
+h5read("example.h5","foo/A")
+
+
+
+### WEB SCRAPING 
+
+con = url("https://scholar.google.com/citations?user=HI-I6C0AAAAJ")
+htmlCode = readLines(con)
+close(con)
+htmlCode
+
+library(XML)
+url <- "https://scholar.google.com/citations?user=HI-I6C0AAAAJ"
+html <- htmlTreeParse(url, useInternalNodes = T)
+xpathSApply(html, "//title", xmlValue)
+
+
+library(httr)
+html2 = GET(url)
+content2 = content(html2, as="text")
+parsedHtml = htmlParse(content2, asText=T)
+xpathSApply(parsedHtml,"//title",xmlValue)
+
+pg1 = GET("http://httpbin.org/basic-auth/user/passwd")
+
+pg2 = GET("http://httpbin.org/basic-auth/user/passwd",authenticate("user","passwd"))
+pg2
+names(pg2)
+
+
+google = handle("http://google.com")
+pg1 = GET(handle=google,path="/")
